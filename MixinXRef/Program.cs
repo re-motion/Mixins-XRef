a59b1a1cef39bbc661f0a5d60071eec610e47be3
Patrick Größ <patrick.groess@rubicon.eu>
2009-08-04 09:50:47 +0000

    Initial revision

---
+++ MixinXRef/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MixinXRef
+{
+  class Program
+  {
+    static void Main (string[] args)
+    {
+    }
+  }
+}
