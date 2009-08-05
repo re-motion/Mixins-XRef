31384b4e9736266b3b610f7daac21b7a41165ece
Patrick Größ <patrick.groess@rubicon.eu>
2009-08-05 13:24:41 +0000

    Replaced fake implementation of InterfaceReportGenerator with real implementation

--- MixinXRef.UnitTests/InterfaceReportGeneratorTest.cs
+++ MixinXRef.UnitTests/InterfaceReportGeneratorTest.cs
@@ -35,26 +35,26 @@ namespace MixinXRef.UnitTests
       Assert.That (output.ToString(), Is.EqualTo (expectedOutput.ToString()));
     }
 
-    //[Test]
-    //public void GenerateXml_WithInterfaces ()
-    //{
-    //  // TargetClass1 implements IDisposable
-    //  var involvedType = new InvolvedType (typeof (TargetClass1));
-    //  _context.InvolvedTypeFinder = new InvolvedTypeFinderStub (involvedType);
-    //  var reportGenerator = new InterfaceReportGenerator (_context);
+    [Test]
+    public void GenerateXml_WithInterfaces ()
+    {
+      // TargetClass1 implements IDisposable
+      var involvedType = new InvolvedType (typeof (TargetClass1));
+      _context.InvolvedTypeFinder = new InvolvedTypeFinderStub (involvedType);
+      var reportGenerator = new InterfaceReportGenerator (_context);
 
-    //  XElement output = reportGenerator.GenerateXml();
+      XElement output = reportGenerator.GenerateXml ();
 
-    //  var expectedOutput = new XElement (
-    //      "Interfaces",
-    //      new XElement (
-    //          "Interface",
-    //          new XAttribute ("id", "0"),
-    //          new XAttribute ("assembly-ref", "0"),
-    //          new XAttribute ("namespace", "System"),
-    //          new XAttribute ("name", "IDisposable")
-    //          ));
-    //  Assert.That (output.ToString(), Is.EqualTo (expectedOutput.ToString()));
-    //}
+      var expectedOutput = new XElement (
+          "Interfaces",
+          new XElement (
+              "Interface",
+              new XAttribute ("id", "0"),
+              new XAttribute ("assembly-ref", "0"),
+              new XAttribute ("namespace", "System"),
+              new XAttribute ("name", "IDisposable")
+              ));
+      Assert.That (output.ToString (), Is.EqualTo (expectedOutput.ToString ()));
+    }
   }
 }
--- MixinXRef/InterfaceReportGenerator.cs
+++ MixinXRef/InterfaceReportGenerator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 using Remotion.Utilities;
 
@@ -16,7 +18,39 @@ namespace MixinXRef
 
     public XElement GenerateXml ()
     {
-      return new XElement ("Interfaces");
+      var allInterfaces = GetAllInterfaces();
+
+
+      return new XElement (
+          "Interfaces",
+          from usedInterface in allInterfaces
+          select
+              GenerateInterfaceElement (usedInterface)
+          );
+    }
+
+    private HashSet<Type> GetAllInterfaces ()
+    {
+      var allInterfaces = new HashSet<Type>();
+
+      foreach (var involvedType in _context.InvolvedTypeFinder.FindInvolvedTypes())
+      {
+        foreach (var usedInterface in involvedType.Type.GetInterfaces())
+          allInterfaces.Add (usedInterface);
+      }
+
+      return allInterfaces;
+    }
+
+    private XElement GenerateInterfaceElement (Type usedInterface)
+    {
+      return new XElement (
+          "Interface",
+          new XAttribute ("id", _context.InterfaceIdentifierGenerator.GetIdentifier (usedInterface)),
+          new XAttribute ("assembly-ref", _context.AssemblyIdentifierGenerator.GetIdentifier (usedInterface.Assembly)),
+          new XAttribute ("namespace", usedInterface.Namespace),
+          new XAttribute ("name", usedInterface.Name)
+          );
     }
   }
 }
