5ddf90aeb933f38456f11aad4ea69650f500270f
Patrick Größ <patrick.groess@rubicon.eu>
2009-08-07 13:08:01 +0000

    Extended TestDomain to have interface introduction

--- MixinXRef.UnitTests/TestDomain/Mixin3.cs
+++ MixinXRef.UnitTests/TestDomain/Mixin3.cs
@@ -1,7 +1,9 @@
 using System;
+using Remotion.Mixins;
 
 namespace MixinXRef.UnitTests.TestDomain
 {
+  [Extends (typeof(TargetClass2))]
   public class Mixin3 : IDisposable
   {
     public void Dispose ()
