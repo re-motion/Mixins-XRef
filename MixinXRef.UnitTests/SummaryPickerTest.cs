14c23471464f70099bfbf154faebb20964288ab1
Patrick Größ <patrick.groess@rubicon.eu>
2009-08-13 07:23:54 +0000

    Added tests for SummaryPicker

--- MixinXRef.UnitTests/SummaryPickerTest.cs
+++ MixinXRef.UnitTests/SummaryPickerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Xml.Linq;
 using MixinXRef.UnitTests.TestDomain;
 using NUnit.Framework;
@@ -40,5 +41,32 @@ namespace MixinXRef.UnitTests
       Assert.That(output.ToString(), Is.EqualTo(expectedOutput.ToString()));
     }
 
+    [Test]
+    public void NormalizeAndTrim_PlainElement ()
+    {
+      XElement output = new XElement ("Test-element", "   test1   \r\n test2 is     Test    ");
+
+      _summaryPicker.NormalizeAndTrim (output);
+
+      var expectedOutput = new XElement ("Test-element", "test1 test2 is Test");
+
+      Assert.That (output.ToString (), Is.EqualTo (expectedOutput.ToString ()));
+    }
+
+    [Test]
+    public void NormalizeAndTrim_WithNestedElements ()
+    {
+      XElement output = new XElement ("Test-element", 
+        "   test   ", 
+        new XElement("innerElement", " test     of   \t inner   \r\nelement   "),
+        " end  "
+        );
+
+      _summaryPicker.NormalizeAndTrim (output);
+
+      var expectedOutput = new XElement ("Test-element", "test", new XElement("innerElement", " test of inner element "), " end");
+
+      Assert.That (output.ToString (), Is.EqualTo (expectedOutput.ToString ()));
+    }
   }
 }
