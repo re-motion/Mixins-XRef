80bd37658f18bb1bf6ede7418a7b789a1ce2f210
Julian Lettner <julian.lettner@rubicon.eu>
2009-09-17 11:12:13 +0000

    [STRG + D] for SummaryPickerTest

--- MixinXRef.UnitTests/SummaryPickerTest.cs
+++ MixinXRef.UnitTests/SummaryPickerTest.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Diagnostics;
 using System.Xml.Linq;
-using MixinXRef.UnitTests.TestDomain;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 using Remotion.Mixins;
@@ -11,7 +9,7 @@ namespace MixinXRef.UnitTests
   [TestFixture]
   public class SummaryPickerTest
   {
-    private readonly XElement noSummary = new XElement ("summary", "No summary found.");
+    private readonly XElement _noSummary = new XElement ("summary", "No summary found.");
     private SummaryPicker _summaryPicker;
 
     [SetUp]
@@ -23,20 +21,22 @@ namespace MixinXRef.UnitTests
     [Test]
     public void GetSummary_ForNonExistingXmlFile ()
     {
-      var output = _summaryPicker.GetSummary(typeof(object));
-      var expectedOutput = noSummary;
+      var output = _summaryPicker.GetSummary (typeof (object));
+      var expectedOutput = _noSummary;
 
       Assert.That (output.ToString(), Is.EqualTo (expectedOutput.ToString()));
     }
 
     [Test]
-    public void GetSummary_ForValidTypeWithXmlFile_SummaryPresent()
+    public void GetSummary_ForValidTypeWithXmlFile_SummaryPresent ()
     {
-      var output = _summaryPicker.GetSummary(typeof(MixinConfiguration));
+      var output = _summaryPicker.GetSummary (typeof (MixinConfiguration));
 
-      var expectedOutput = new XElement("summary", "Constitutes a mixin configuration (ie. a set of classes associated with mixins) and manages the mixin configuration for the current thread.");
+      var expectedOutput = new XElement (
+          "summary",
+          "Constitutes a mixin configuration (ie. a set of classes associated with mixins) and manages the mixin configuration for the current thread.");
 
-      Assert.That(output.ToString(), Is.EqualTo(expectedOutput.ToString()));
+      Assert.That (output.ToString(), Is.EqualTo (expectedOutput.ToString()));
     }
 
     [Test]
@@ -48,23 +48,24 @@ namespace MixinXRef.UnitTests
 
       var expectedOutput = new XElement ("TestElement", "test1 test2 is Test");
 
-      Assert.That (output.ToString (), Is.EqualTo (expectedOutput.ToString ()));
+      Assert.That (output.ToString(), Is.EqualTo (expectedOutput.ToString()));
     }
 
     [Test]
     public void NormalizeAndTrim_WithNestedElements ()
     {
-      var element1 = new XElement ("OuterElement", 
-        "   test   ", 
-        new XElement("innerElement", " test     of   \t inner   \r\nelement   "),
-        "end  "
-        );
+      var element1 = new XElement (
+          "OuterElement",
+          "   test   ",
+          new XElement ("innerElement", " test     of   \t inner   \r\nelement   "),
+          "end  "
+          );
 
       var output = _summaryPicker.NormalizeAndTrim (element1);
 
-      var expectedOutput = new XElement("OuterElement", "test", new XElement("innerElement", "test of inner element"), "end");
+      var expectedOutput = new XElement ("OuterElement", "test", new XElement ("innerElement", "test of inner element"), "end");
 
-      Assert.That (output.ToString (), Is.EqualTo (expectedOutput.ToString ()));
+      Assert.That (output.ToString(), Is.EqualTo (expectedOutput.ToString()));
     }
   }
 }
