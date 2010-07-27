81a9f199b07ef3d4a63aafbdb9d00e063ee29849
Patrick Größ <patrick.groess@rubicon.eu>
2010-07-27 09:09:38 +0000

    Added project MixinXRefGUI

---
+++ MixinXRefGUI/MixinXRefForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace MixinXRefGUI
+{
+  partial class MixinXRefForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose (bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose ();
+      }
+      base.Dispose (disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent ()
+    {
+      this.components = new System.ComponentModel.Container ();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager (typeof (MixinXRefForm));
+      this.forceOverrideCheckBox = new System.Windows.Forms.CheckBox ();
+      this.browseAssemblyPath = new System.Windows.Forms.Button ();
+      this.assemblyPathTextBox = new System.Windows.Forms.TextBox ();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox ();
+      this.outputPathTextBox = new System.Windows.Forms.TextBox ();
+      this.browseOutputPath = new System.Windows.Forms.Button ();
+      this.customReflectorTextBox = new System.Windows.Forms.TextBox ();
+      this.label1 = new System.Windows.Forms.Label ();
+      this.label2 = new System.Windows.Forms.Label ();
+      this.label3 = new System.Windows.Forms.Label ();
+      this.startMixinXRefButton = new System.Windows.Forms.Button ();
+      this.logTextBox = new System.Windows.Forms.TextBox ();
+      this.cursorIconTimer = new System.Windows.Forms.Timer (this.components);
+      this.toolTip1 = new System.Windows.Forms.ToolTip (this.components);
+      ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit ();
+      this.SuspendLayout ();
+      // 
+      // forceOverrideCheckBox
+      // 
+      this.forceOverrideCheckBox.AutoSize = true;
+      this.forceOverrideCheckBox.Location = new System.Drawing.Point (12, 101);
+      this.forceOverrideCheckBox.Name = "forceOverrideCheckBox";
+      this.forceOverrideCheckBox.Size = new System.Drawing.Size (131, 17);
+      this.forceOverrideCheckBox.TabIndex = 0;
+      this.forceOverrideCheckBox.Text = "Replace existing files?";
+      this.forceOverrideCheckBox.UseVisualStyleBackColor = true;
+      // 
+      // browseAssemblyPath
+      // 
+      this.browseAssemblyPath.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.browseAssemblyPath.Location = new System.Drawing.Point (229, 28);
+      this.browseAssemblyPath.Name = "browseAssemblyPath";
+      this.browseAssemblyPath.Size = new System.Drawing.Size (54, 20);
+      this.browseAssemblyPath.TabIndex = 1;
+      this.browseAssemblyPath.Text = "Browse";
+      this.browseAssemblyPath.UseVisualStyleBackColor = true;
+      this.browseAssemblyPath.Click += new System.EventHandler (this.BrowseAssemblyPath_Click);
+      // 
+      // assemblyPathTextBox
+      // 
+      this.assemblyPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.assemblyPathTextBox.Location = new System.Drawing.Point (12, 29);
+      this.assemblyPathTextBox.Name = "assemblyPathTextBox";
+      this.assemblyPathTextBox.Size = new System.Drawing.Size (211, 20);
+      this.assemblyPathTextBox.TabIndex = 2;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject ("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point (307, 26);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size (154, 69);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+      this.pictureBox1.TabIndex = 3;
+      this.pictureBox1.TabStop = false;
+      // 
+      // outputPathTextBox
+      // 
+      this.outputPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.outputPathTextBox.Location = new System.Drawing.Point (12, 75);
+      this.outputPathTextBox.Name = "outputPathTextBox";
+      this.outputPathTextBox.Size = new System.Drawing.Size (211, 20);
+      this.outputPathTextBox.TabIndex = 5;
+      // 
+      // browseOutputPath
+      // 
+      this.browseOutputPath.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.browseOutputPath.Location = new System.Drawing.Point (229, 75);
+      this.browseOutputPath.Name = "browseOutputPath";
+      this.browseOutputPath.Size = new System.Drawing.Size (54, 20);
+      this.browseOutputPath.TabIndex = 4;
+      this.browseOutputPath.Text = "Browse";
+      this.browseOutputPath.UseVisualStyleBackColor = true;
+      this.browseOutputPath.Click += new System.EventHandler (this.BrowseOutputPath_Click);
+      // 
+      // customReflectorTextBox
+      // 
+      this.customReflectorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.customReflectorTextBox.Location = new System.Drawing.Point (12, 144);
+      this.customReflectorTextBox.Name = "customReflectorTextBox";
+      this.customReflectorTextBox.Size = new System.Drawing.Size (271, 20);
+      this.customReflectorTextBox.TabIndex = 7;
+      this.toolTip1.SetToolTip (this.customReflectorTextBox, "FullQualifiedClassName, FullQualifiedAssemblyName");
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point (12, 10);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size (74, 13);
+      this.label1.TabIndex = 8;
+      this.label1.Text = "Input directory";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point (12, 59);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size (82, 13);
+      this.label2.TabIndex = 8;
+      this.label2.Text = "Output directory";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Font = new System.Drawing.Font ("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+      this.label3.Location = new System.Drawing.Point (12, 131);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size (129, 13);
+      this.label3.TabIndex = 8;
+      this.label3.Text = "Custom reflector (optional)";
+      this.toolTip1.SetToolTip (this.label3, "The custom reflector has to be in the input directory");
+      // 
+      // startMixinXRefButton
+      // 
+      this.startMixinXRefButton.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.startMixinXRefButton.Location = new System.Drawing.Point (307, 131);
+      this.startMixinXRefButton.Name = "startMixinXRefButton";
+      this.startMixinXRefButton.Size = new System.Drawing.Size (154, 33);
+      this.startMixinXRefButton.TabIndex = 9;
+      this.startMixinXRefButton.Text = "Start Mixin-Cross-Referencer";
+      this.startMixinXRefButton.UseVisualStyleBackColor = true;
+      this.startMixinXRefButton.Click += new System.EventHandler (this.StartMixinXRefButton_Click);
+      // 
+      // logTextBox
+      // 
+      this.logTextBox.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.logTextBox.Location = new System.Drawing.Point (15, 192);
+      this.logTextBox.Multiline = true;
+      this.logTextBox.Name = "logTextBox";
+      this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.logTextBox.Size = new System.Drawing.Size (446, 116);
+      this.logTextBox.TabIndex = 10;
+      // 
+      // cursorIconTimer
+      // 
+      this.cursorIconTimer.Tick += new System.EventHandler (this.CursorIconTimer_Tick);
+      // 
+      // MixinXRefForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size (480, 326);
+      this.Controls.Add (this.logTextBox);
+      this.Controls.Add (this.startMixinXRefButton);
+      this.Controls.Add (this.label3);
+      this.Controls.Add (this.label2);
+      this.Controls.Add (this.label1);
+      this.Controls.Add (this.customReflectorTextBox);
+      this.Controls.Add (this.outputPathTextBox);
+      this.Controls.Add (this.browseOutputPath);
+      this.Controls.Add (this.pictureBox1);
+      this.Controls.Add (this.assemblyPathTextBox);
+      this.Controls.Add (this.browseAssemblyPath);
+      this.Controls.Add (this.forceOverrideCheckBox);
+      this.Icon = ((System.Drawing.Icon) (resources.GetObject ("$this.Icon")));
+      this.Name = "MixinXRefForm";
+      this.Text = "MixinXRef";
+      ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit ();
+      this.ResumeLayout (false);
+      this.PerformLayout ();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.CheckBox forceOverrideCheckBox;
+    private System.Windows.Forms.Button browseAssemblyPath;
+    private System.Windows.Forms.TextBox assemblyPathTextBox;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.TextBox outputPathTextBox;
+    private System.Windows.Forms.Button browseOutputPath;
+    private System.Windows.Forms.TextBox customReflectorTextBox;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Button startMixinXRefButton;
+    private System.Windows.Forms.TextBox logTextBox;
+    private System.Windows.Forms.Timer cursorIconTimer;
+    private System.Windows.Forms.ToolTip toolTip1;
+  }
+}
+
---
+++ MixinXRefGUI/MixinXRefForm.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace MixinXRefGUI
+{
+  public partial class MixinXRefForm : Form
+  {
+    private readonly string _persistentSettingsFile;
+
+    private delegate void AppendTextToLogTextBoxAsyncDelegate (string message);
+
+    private delegate void setStartMixinRefButtonEnabledDelegate (bool enabled);
+
+    public MixinXRefForm ()
+    {
+      InitializeComponent();
+
+      var baseDir = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData), "MixinXRef");
+      _persistentSettingsFile = Path.Combine (baseDir, "MixinXRefGUI.dat");
+
+      GetOrCreateSettings (_persistentSettingsFile);
+    }
+
+    private void GetOrCreateSettings (string persistentSettingsFile)
+    {
+      if (!Directory.Exists (Path.GetDirectoryName (_persistentSettingsFile)))
+        CreateDefaultSettings (persistentSettingsFile);
+
+      LoadSettings (persistentSettingsFile);
+    }
+
+    private void CreateDefaultSettings (string persistentSettingsFile)
+    {
+      try
+      {
+        var baseDirectory = Path.GetDirectoryName (_persistentSettingsFile);
+        Directory.CreateDirectory (baseDirectory);
+        File.WriteAllLines (persistentSettingsFile, new[] { @"C:\", @"C:\", "", @"False" });
+      }
+      catch (Exception exception)
+      {
+        //File.WriteAllText (@".\MixinXRefGUI.log", exception.Message);
+        MessageBox.Show (exception.Message, "Failed to save settings.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+      }
+    }
+
+    private void LoadSettings (string persistentSettingsFile)
+    {
+      if (File.Exists (persistentSettingsFile))
+      {
+        var settings = File.ReadAllLines (persistentSettingsFile);
+        if (settings.Length == 4)
+        {
+          if (Directory.Exists (settings[0]))
+            assemblyPathTextBox.Text = settings[0];
+          if (Directory.Exists (settings[1]))
+            outputPathTextBox.Text = settings[1];
+
+          customReflectorTextBox.Text = settings[2];
+
+          forceOverrideCheckBox.Checked = bool.Parse (settings[3]);
+        }
+      }
+    }
+
+    private void BrowseAssemblyPath_Click (object sender, EventArgs e)
+    {
+      ShowAndSetFolderBrowserDialogForTextBox ("Select input folder", assemblyPathTextBox);
+    }
+
+    private void BrowseOutputPath_Click (object sender, EventArgs e)
+    {
+      ShowAndSetFolderBrowserDialogForTextBox ("Select output folder", outputPathTextBox);
+    }
+
+    private void ShowAndSetFolderBrowserDialogForTextBox (string description, TextBox pathTextBox)
+    {
+      var folderBrowserDialog = new FolderBrowserDialog { Description = description };
+
+      if (Directory.Exists (pathTextBox.Text))
+        folderBrowserDialog.SelectedPath = Path.GetFullPath (pathTextBox.Text);
+
+      var objResult = folderBrowserDialog.ShowDialog (this);
+      if (objResult == DialogResult.OK)
+        pathTextBox.Text = folderBrowserDialog.SelectedPath;
+    }
+
+    private void StartMixinXRefButton_Click (object sender, EventArgs e)
+    {
+      SaveSettings();
+
+      var remotionAssembly = Path.Combine (assemblyPathTextBox.Text, "Remotion.dll");
+      if (File.Exists (remotionAssembly))
+        StartMixinXRefApplication();
+      else
+      {
+        MessageBox.Show (
+            "The input directory doesn't contain the remotion assembly. Please select another input directory.",
+            "Invalid input directory",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error);
+      }
+    }
+
+    private void StartMixinXRefApplication ()
+    {
+      var xRefPath = Path.GetDirectoryName (Assembly.GetExecutingAssembly().Location);
+      var fileName = Path.Combine (xRefPath, "MixinXRef.exe");
+      var arguments = GetArguments();
+
+      Cursor = Cursors.AppStarting;
+      startMixinXRefButton.Enabled = false;
+
+      var process = new Process();
+      process.StartInfo.FileName = fileName;
+      process.StartInfo.Arguments = arguments;
+      process.StartInfo.UseShellExecute = false;
+      process.StartInfo.CreateNoWindow = true;
+      process.StartInfo.RedirectStandardError = true;
+      process.StartInfo.RedirectStandardOutput = true;
+
+      process.OutputDataReceived += WriteProcessStream;
+      process.ErrorDataReceived += WriteProcessStream;
+      process.EnableRaisingEvents = true;
+      process.Exited += ProcessExited;
+      process.Disposed += ProcessExited;
+
+      logTextBox.Text = "Started function.  Please stand by.." + Environment.NewLine;
+
+      process.Start();
+      process.BeginOutputReadLine();
+      process.BeginErrorReadLine();
+
+      cursorIconTimer.Enabled = true;
+    }
+
+    private string GetArguments ()
+    {
+      var arguments = String.Format ("\"{0}\" \"{1}\"", assemblyPathTextBox.Text, outputPathTextBox.Text);
+
+      if (!String.IsNullOrEmpty (customReflectorTextBox.Text))
+        arguments += " \"" + customReflectorTextBox.Text + "\"";
+
+      if (forceOverrideCheckBox.Checked)
+        arguments += " -force";
+
+      return arguments;
+    }
+
+    private void SaveSettings ()
+    {
+      File.WriteAllLines (
+          _persistentSettingsFile,
+          new[]
+          {
+              assemblyPathTextBox.Text,
+              outputPathTextBox.Text,
+              customReflectorTextBox.Text,
+              forceOverrideCheckBox.Checked.ToString()
+          }
+          );
+    }
+
+    private void AppendTextToLogTextBoxAsync (string message)
+    {
+      if (logTextBox.InvokeRequired)
+        logTextBox.Invoke (new AppendTextToLogTextBoxAsyncDelegate (AppendTextToLogTextBoxAsync), new object[] { message });
+      else
+        logTextBox.AppendText (message + Environment.NewLine);
+    }
+
+    private void SetStartMixinRefButtonEnabled (bool enabled)
+    {
+      if (startMixinXRefButton.InvokeRequired)
+        startMixinXRefButton.Invoke (new setStartMixinRefButtonEnabledDelegate (SetStartMixinRefButtonEnabled), new object[] { enabled });
+      else
+        startMixinXRefButton.Enabled = enabled;
+    }
+
+    private void WriteProcessStream (object sender, DataReceivedEventArgs dataReceivedEventArgs)
+    {
+      if (!String.IsNullOrEmpty (dataReceivedEventArgs.Data))
+        AppendTextToLogTextBoxAsync (dataReceivedEventArgs.Data);
+    }
+
+    private void ProcessExited (object sender, EventArgs e)
+    {
+      SetStartMixinRefButtonEnabled (true);
+    }
+
+    private void CursorIconTimer_Tick (object sender, EventArgs e)
+    {
+      if (startMixinXRefButton.Enabled)
+      {
+        Cursor = Cursors.Default;
+        cursorIconTimer.Enabled = false;
+      }
+    }
+  }
+}
