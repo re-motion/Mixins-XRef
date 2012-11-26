d656af7b1a31084fc003570f7919e294ae797b5a
Dominik Rauch <dominik.rauch@rubicon.eu>
2012-11-26 12:01:37 +0000

    Added IntegrationTest project for binding redirects testing.

---
+++ IntegrationTests/WebAppWithWebConfigTest/TestMixin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MixinXRef.UnitTests.TestDomain;
+using Remotion.Mixins;
+
+namespace WebAppWithWebConfigTest
+{
+  public class TestMixin : Mixin<IDoSomething>
+  {
+    [OverrideTarget]
+    public void DoSomething ()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
