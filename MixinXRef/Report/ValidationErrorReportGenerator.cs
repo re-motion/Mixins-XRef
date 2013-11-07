33a9b3e7fa07e5673a82e10bc36647aad2fde05c
Tymon Terlikiewicz <tymon.terlikiewicz@rubicon.eu>
2013-11-07 16:53:56 +0000

    temporarily disabled validation report generation.

--- MixinXRef/Report/ValidationErrorReportGenerator.cs
+++ MixinXRef/Report/ValidationErrorReportGenerator.cs
@@ -44,16 +44,15 @@ namespace MixinXRef.Report
       foreach (var exception in _errorAggregator.Exceptions)
       {
         var topLevelExceptionElement = new RecursiveExceptionReportGenerator (exception).GenerateXml();
-        var validationLog = _remotionReflector.GetValidationLogFromValidationException(exception);
 
         topLevelExceptionElement.Add (
             new XElement (
                 "ValidationLog",
-                new XAttribute("number-of-rules-executed", validationLog.CallMethod("GetNumberOfRulesExecuted")),
-                new XAttribute("number-of-failures", validationLog.CallMethod("GetNumberOfFailures")),
-                new XAttribute("number-of-unexpected-exceptions", validationLog.CallMethod("GetNumberOfUnexpectedExceptions")),
-                new XAttribute("number-of-warnings", validationLog.CallMethod("GetNumberOfWarnings")),
-                new XAttribute("number-of-successes", validationLog.CallMethod("GetNumberOfSuccesses"))
+                new XAttribute("number-of-rules-executed", 0),
+                new XAttribute("number-of-failures", 0),
+                new XAttribute("number-of-unexpected-exceptions", 0),
+                new XAttribute("number-of-warnings", 0),
+                new XAttribute("number-of-successes", 0)
                 )
             );
         validationErrors.Add (topLevelExceptionElement);
