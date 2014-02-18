be39a95d1d4b59ab28d48841aa2ff3db3ef7bcce
Tymon Terlikiewicz <tymon.terlikiewicz@rubicon.eu>
2014-02-18 11:06:00 +0000

    fix for buildscript version info regex parsing

--- AssemblyInfoShared.cs
+++ AssemblyInfoShared.cs
@@ -36,9 +36,10 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion ("2.0.5.0")] //major and minor version for official releases are configured in the team city build parameters
-[assembly: AssemblyFileVersion ("2.0.5.0")] //major and minor version for official releases are configured in the team city build parameters
+
+//major and minor version for official releases are configured in the team city build parameters:
+
+[assembly: AssemblyVersion ("2.0.5.0")]
+[assembly: AssemblyFileVersion ("2.0.5.0")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyInformationalVersion ("")]
-
-
