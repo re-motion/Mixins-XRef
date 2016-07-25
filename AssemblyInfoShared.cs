cee39d845f2558b5cd59a17493cb7fac4aeef93c
Michael Ketting <michael.ketting@rubicon.eu>
2016-07-25 16:41:08 +0200

    Install Remotion.BuildScript and Remotion.ReleaseProcessScript v2.0.0-alpha.12

--- AssemblyInfoShared.cs
+++ AssemblyInfoShared.cs
@@ -18,27 +18,11 @@
 using System;
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyCompany ("rubicon IT GmbH")]
 [assembly: AssemblyProduct ("MixinXRef")]
-[assembly: AssemblyCopyright ("(c) rubicon IT GmbH, www.rubicon.eu - Licensed under GNU Lesser General Public License")]
+[assembly: AssemblyCopyright ("Copyright (c) rubicon IT GmbH, www.rubicon.eu - Licensed under GNU LGPL 2.1 (or later)")]
 [assembly: AssemblyTrademark ("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion ("1.9.4.0")]
-[assembly: AssemblyFileVersion ("1.9.4.0")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyInformationalVersion ("")]
-
-
+[assembly: AssemblyVersion ("1.9.0.0")]
+[assembly: AssemblyFileVersion ("1.9.5.24009")]
+[assembly: AssemblyConfiguration ("Build type: Debug")]
+[assembly: AssemblyInformationalVersion ("1.9.5-x.9+Debug")]
